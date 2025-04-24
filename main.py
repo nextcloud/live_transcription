8e4e1f3740cc3e44e25e146e1bac74738c2f8a88
Anupam Kumar <kyteinsky@gmail.com>
2025-04-24 13:41:23 +0530

    tidy up
    Signed-off-by: Anupam Kumar <kyteinsky@gmail.com>

--- main.py
+++ main.py
@@ -1,26 +1,19 @@
 import asyncio
 import base64
-import datetime
 import hashlib
 import hmac
 import json
-import math
 import os
 import pprint
 import random
-from contextlib import suppress
 from enum import Enum, IntEnum
 from threading import Event
 
-import cv2
 import httpx
-import numpy
-from aiortc import RTCPeerConnection, RTCSessionDescription, VideoStreamTrack
-from aiortc.contrib.media import MediaBlackhole, MediaPlayer, MediaRecorder
-from aiortc.contrib.signaling import BYE, TcpSocketSignaling, add_signaling_arguments, create_signaling
+from aiortc import RTCPeerConnection, RTCSessionDescription
+from aiortc.contrib.media import MediaRecorder
 from aiortc.rtcconfiguration import RTCConfiguration, RTCIceServer
 from aiortc.sdp import candidate_from_sdp
-from av import VideoFrame
 from dotenv import load_dotenv
 from print_color import print
 from pydantic import BaseModel
@@ -135,7 +128,7 @@ class WebSocketSignaling:
 				},
 				"data": {
 					"type": "requestoffer",
-					"roomType": "video" # todo: check audio
+					"roomType": "video"
 				}
 			}
 		})
@@ -205,128 +198,12 @@ class WebSocketSignaling:
 		return message
 
 
-class FlagVideoStreamTrack(VideoStreamTrack):
-	"""
-	A video track that returns an animated flag.
-	"""
-
-	def __init__(self):
-		super().__init__()  # don't forget this!
-		self.counter = 0
-		height, width = 480, 640
-
-		# generate flag
-		data_bgr = numpy.hstack(
-			[
-				self._create_rectangle(
-					width=213, height=480, color=(255, 0, 0)
-				),  # blue
-				self._create_rectangle(
-					width=214, height=480, color=(255, 255, 255)
-				),  # white
-				self._create_rectangle(width=213, height=480, color=(0, 0, 255)),  # red
-			]
-		)
-
-		# shrink and center it
-		M = numpy.float32([[0.5, 0, width / 4], [0, 0.5, height / 4]])
-		data_bgr = cv2.warpAffine(data_bgr, M, (width, height))
-
-		# compute animation
-		omega = 2 * math.pi / height
-		id_x = numpy.tile(numpy.array(range(width), dtype=numpy.float32), (height, 1))
-		id_y = numpy.tile(
-			numpy.array(range(height), dtype=numpy.float32), (width, 1)
-		).transpose()
-
-		self.frames = []
-		for k in range(30):
-			phase = 2 * k * math.pi / 30
-			map_x = id_x + 10 * numpy.cos(omega * id_x + phase)
-			map_y = id_y + 10 * numpy.sin(omega * id_x + phase)
-			self.frames.append(
-				VideoFrame.from_ndarray(
-					cv2.remap(data_bgr, map_x, map_y, cv2.INTER_LINEAR), format="bgr24"
-				)
-			)
-
-	async def recv(self):
-		pts, time_base = await self.next_timestamp()
-
-		frame = self.frames[self.counter % 30]
-		frame.pts = pts
-		frame.time_base = time_base
-		self.counter += 1
-		return frame
-
-	def _create_rectangle(self, width, height, color):
-		data_bgr = numpy.zeros((height, width, 3), numpy.uint8)
-		data_bgr[:, :] = color
-		return data_bgr
-
-
-class VideoReceiver:
-	def __init__(self):
-		self.track = None
-
-	async def handle_track(self, track):
-		print("Inside handle track")
-		self.track = track
-		frame_count = 0
-		while True:
-			try:
-				print("Waiting for frame...")
-				frame = await asyncio.wait_for(track.recv(), timeout=5.0)
-				frame_count += 1
-				print(f"Received frame {frame_count}")
-				
-				if isinstance(frame, VideoFrame):
-					print(f"Frame type: VideoFrame, pts: {frame.pts}, time_base: {frame.time_base}")
-					frame = frame.to_ndarray(format="bgr24")
-				elif isinstance(frame, numpy.ndarray):
-					print("Frame type: numpy array")
-				else:
-					print(f"Unexpected frame type: {type(frame)}")
-					continue
-
-				# Add timestamp to the frame
-				current_time = datetime.now()
-				new_time = current_time - datetime.timedelta( seconds=55)
-				timestamp = new_time.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
-				cv2.putText(frame, timestamp, (10, frame.shape[0] - 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-				cv2.imwrite(f"imgs/received_frame_{frame_count}.jpg", frame)
-				print(f"Saved frame {frame_count} to file")
-				cv2.imshow("Frame", frame)
-
-				# Exit on 'q' key press
-				if cv2.waitKey(1) & 0xFF == ord('q'):
-						break
-			except asyncio.TimeoutError:
-					print("Timeout waiting for frame, continuing...")
-			except Exception as e:
-					print(f"Error in handle_track: {str(e)}")
-					if "Connection" in str(e):
-							break
-	print("Exiting handle_track")
-
-
 async def run(pc, signaling, recorder):
 	@pc.on("track")
 	async def on_track(track):
 		print("Receiving %s" % track.kind, tag='track', color='magenta')
-		# if track.kind == "video" or track.kind == "audio":
 		if track.kind == "audio":
 			recorder.addTrack(track)
-			# await asyncio.ensure_future(recorder.handle_track(track))
-			# await recorder.handle_track(track)
-		# if track.kind == "audio":
-		# 	recorder.addTrack(track)
-		# else:
-		# 	print("Unknown track kind: %s" % track.kind, tag='track', color='red')
-
-	# @pc.on('iceconnectionstatechange')
-	# async def on_iceconnectionstatechange():
-	# 	print("ICE connection state is", pc.iceConnectionState, tag='ice', color='blue')
 
 	@pc.on('connectionstatechange')
 	async def on_connectionstatechange():
@@ -347,26 +224,16 @@ async def run(pc, signaling, recorder):
 	#await pc.setLocalDescription(await pc.createOffer())
 	#await signaling.send(pc.localDescription)
 
-	# consume signaling
-	# while True:
-
-	# while True:
-		# try:
-		# 	message = await signaling.receive()
-		# 	if not message:
-		# 		raise Exception("No message received")
-		# except Exception:
-		# 	print('Connection closed', tag='connection', color='red')
-		# 	break
-
 	await signaling.connect()
 	await signaling.send_join('i6h8x8q9')
 	pc.addTransceiver('audio', direction='recvonly')
-	# pc.addTransceiver('video', direction='recvonly')
 
 	print('Connected to signaling server')
 
 	while True:
+		if not app_started.is_set():
+			break
+
 		try:
 			message = await signaling.receive()
 		except ConnectionClosedOK:
@@ -378,8 +245,6 @@ async def run(pc, signaling, recorder):
 			for user_description in message['event']['update']['users']:
 				if user_description['inCall'] & CALL_FLAG.IN_CALL and user_description['inCall'] & CALL_FLAG.WITH_AUDIO:
 					print('User join with audio', user_description, tag='participants', color='blue')
-					# pc.addTransceiver('audio', direction='recvonly')
-					# pc.addTransceiver('video', direction='recvonly')
 					targets[user_description['sessionId']] = Target(
 						session_id=user_description['sessionId'],
 						raw_message=user_description,
@@ -421,7 +286,6 @@ async def run(pc, signaling, recorder):
 
 
 if __name__ == "__main__":
-	# todo: call spreed for ice servers
 	# https://nuage.minifox.fr/call/i6h8x8q9
 
 	res = httpx.get('https://nuage.minifox.fr/ocs/v2.php/apps/spreed/api/v3/signaling/settings',
@@ -445,7 +309,6 @@ if __name__ == "__main__":
 	app_started.set()
 	signaling = WebSocketSignaling(
 		'wss://hpb.pluton.minifox.fr/standalone-signaling/spreed',
-		# 'https://nuage.minifox.fr',
 		'https://nuage.minifox.fr/ocs/v2.php/apps/spreed/api/v3/signaling/backend',
 		os.environ["SIGNALLING_SECRET"],
 	)
@@ -462,7 +325,6 @@ if __name__ == "__main__":
 	)
 	pc = RTCPeerConnection(configuration=ice_servers)
 	recorder = MediaRecorder("/home/tyrell/nextcloud-docker-dev/workspace/live-transcription-tests/output.ogg")
-	# recorder = VideoReceiver()
 
 	# run event loop
 	loop = asyncio.get_event_loop()
