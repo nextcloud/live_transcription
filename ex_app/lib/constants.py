b956f626dd337177eed4e7801268b9bba7591fcc
Anupam Kumar <kyteinsky@gmail.com>
2025-10-01 14:35:13 +0200

    chore: constants update for more room for systems to get ready
    Signed-off-by: Anupam Kumar <kyteinsky@gmail.com>

--- ex_app/lib/constants.py
+++ ex_app/lib/constants.py
@@ -8,7 +8,7 @@ MAX_CONNECT_TRIES = 5  # maximum number of connection attempts
 MAX_AUDIO_FRAMES = 20  # maximum number of audio frames to collect before sending to Vosk
 MIN_TRANSCRIPT_SEND_INTERVAL = 0.3  # min seconds to wait before sending a partial transcript again
 HPB_SHUTDOWN_TIMEOUT = 30  # seconds to wait for the ws connectino to shut down gracefully
-CALL_LEAVE_TIMEOUT = 60  # seconds to wait before leaving the call if there are no targets
+CALL_LEAVE_TIMEOUT = 120  # seconds to wait before leaving the call if there are no targets
 # wait VOSK_CONNECT_TIMEOUT seconds for the Vosk server handshake to complete,
 # this includes the language load time in the Vosk server
-VOSK_CONNECT_TIMEOUT = 60
+VOSK_CONNECT_TIMEOUT = 120
